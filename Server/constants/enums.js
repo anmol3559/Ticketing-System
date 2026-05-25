a9d9216e39af68e086704d762b8bc512540386de
Anmol Tyagi <anmoltyagi3559@gmail.com>
2026-05-25 21:06:55 +0530

    feat(constants): add global application constants and enums

---
+++ Server/constants/enums.js
@@ -0,0 +1,25 @@
+const login ={
+    REGISTER : "Register",
+    LOGIN : "Login",
+    LOGOUT : "Logout",
+    TICKET_CREATED : "Ticket Created",
+    TICKET_STATUS_CHANGED : "Ticket Status Changed"
+}
+
+const Persona = {
+    User:"User",
+    Admin:"Admin"
+}
+
+const Ticket_status = {
+    OPEN: "Open",
+    CLOSED: "Closed",
+    RESOLVED: "Resolved",
+    REJECTED: "Rejected"
+}
+
+const Ticket_priority = {
+    LOW: "Low",
+    MEDIUM: "Medium",
+    HIGH: "High"
+}
